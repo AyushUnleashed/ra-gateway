1543d0c327ba56079858a3ec20b0772439c16d12
AyushUnleashed <myzealouslifeofficial@gmail.com>
2024-09-15 01:43:39 +0530

    updated supbase tools

--- src/supabase_tools/handle_actor_tb_updates.py
+++ src/supabase_tools/handle_actor_tb_updates.py
@@ -13,3 +13,12 @@ def get_actor_from_db(actor_id: UUID) -> Actor:
     response = SUPABASE_CLIENT.table(TableNames.ACTORS).select("*").eq("id", str(actor_id)).single().execute()
     actor_data = response.data
     return Actor(**actor_data)
+
+if __name__ == "__main__":
+    actors = get_actors_from_db()
+    print(f"Retrieved {len(actors)} actors from the database.")
+    
+    if actors:
+        actor_id = actors[0].id
+        actor = get_actor_from_db(actor_id)
+        print(f"Retrieved actor with ID {actor_id}: {actor}")
--- src/supabase_tools/handle_layout_tb_updates.py
+++ src/supabase_tools/handle_layout_tb_updates.py
@@ -2,13 +2,23 @@ from src.models.base_models import VideoLayout
 from typing import List
 from uuid import UUID
 from src.supabase_tools.supabase_client import SUPABASE_CLIENT
+from src.utils.constants import TableNames
 
 def get_layouts_from_db() -> List[VideoLayout]:
-    response = SUPABASE_CLIENT.table("layouts").select("*").execute()
+    response = SUPABASE_CLIENT.table(TableNames.VIDEO_LAYOUTS).select("*").execute()
     layouts_data = response.data
     return [VideoLayout(**layout) for layout in layouts_data]
 
 def get_layout_from_db(layout_id: UUID) -> VideoLayout:
-    response = SUPABASE_CLIENT.table("layouts").select("*").eq("id", str(layout_id)).single().execute()
+    response = SUPABASE_CLIENT.table(TableNames.VIDEO_LAYOUTS).select("*").eq("id", str(layout_id)).single().execute()
     layout_data = response.data
     return VideoLayout(**layout_data)
+
+if __name__ == "__main__":
+    layouts = get_layouts_from_db()
+    print(f"Retrieved {len(layouts)} layouts from the database.")
+    
+    if layouts:
+        layout_id = layouts[0].id
+        layout = get_layout_from_db(layout_id)
+        print(f"Retrieved layout with ID {layout_id}: {layout}")
