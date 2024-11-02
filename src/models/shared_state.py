6b6b42f5137241dd731815c2546c940471189b94
AyushUnleashed <myzealouslifeofficial@gmail.com>
2024-11-03 01:53:02 +0530

    Replaced shared state dict with LRU cache

--- src/models/shared_state.py
+++ src/models/shared_state.py
@@ -1,6 +1,7 @@
 from typing import Dict
 from uuid import UUID
 from src.models.base_models import Project
+from cachetools import LRUCache
 
-# Global dictionary to store projects in memory
-projects_in_memory: Dict[UUID, Project] = {}
+# Use LRUCache to store projects in memory with a maximum size of 50
+projects_in_memory: Dict[UUID, Project] = LRUCache(maxsize=100)
