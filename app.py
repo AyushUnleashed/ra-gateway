962a10aa73102b633692701a92357d22c123a8e6
AyushUnleashed <myzealouslifeofficial@gmail.com>
2024-11-17 19:59:32 +0530

    refactored endpoints

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from src.config import Config
+from src.config.settings import Settings
 
 
 sentry_sdk.init(
-    dsn=Config.SENTRY_DSN,
+    dsn=Settings.SENTRY_DSN,
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for tracing.
     traces_sample_rate=1.0,
-    environment=Config.APP_ENV,
+    environment=Settings.APP_ENV,
     # Set profiles_sample_rate to 1.0 to profile 100%
     # of sampled transactions.
     # We recommend adjusting this value in production.
@@ -43,10 +43,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-from src.api.main_routes import main_router
-from src.api.webhook_routes import webhook_router
+from src.api.routes.main_routes import main_router
+from src.api.routes.actor_routes import actors_router
+from src.api.routes.products_routes import products_router
+from src.api.routes.projects_routes import projects_router
+from src.api.routes.scripts_routes import scripts_router
+from src.api.routes.users_routes import users_router
+from src.api.routes.video_layouts_routes import video_layouts_router
+from src.api.routes.webhook_routes import webhook_router
 
 app.include_router(main_router)
+app.include_router(actors_router)
+app.include_router(products_router)
+app.include_router(projects_router)
+app.include_router(scripts_router)
+app.include_router(users_router)
+app.include_router(video_layouts_router)
 app.include_router(webhook_router)
 
 @app.get("/sentry-debug")
